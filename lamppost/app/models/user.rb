31deee99c3a09c995d8a69e066d418b91f94923d
Kit Stone <kitstone78@gmail.com>
2014-07-26 20:06:51 -0400

    Added validations and associations to user model.

--- lamppost/app/models/user.rb
+++ lamppost/app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ActiveRecord::Base
+  has_many :messages
+
+  before_validation :downcase_email
+
+  validates :username, 
+            presence: { :message => "Username is required!" }
+            
+  validates :username,
+            uniqueness: { :message => "That username is aready being taken. " }
+  
+  validates_length_of :username, 
+                      :minimum => 2,
+                      :message => "Username must be at least two characters."
+
+  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
+  validates :password, presence: true
+  validates_length_of :password, :minimum => 8
+
+  validates_format_of :email,
+                      :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+                      :message => "Invalid Email"
+
+  def downcase_email
+      self.email = email.to_s.downcase
+  end
 end
