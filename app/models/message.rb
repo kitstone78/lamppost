052792c603dedd4629a9399c056cefbed69cfaa9
Sean Flaherty <sean.flaherty@gmail.com>
2014-08-02 18:38:28 -0400

    Add a validation to the message model requiring a user_id before a save or update.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :user
   
 #TODO KAS Figure out how to have a user create a message
+  validates :user_id, presence: true
   validates :text, presence: true
   validates_length_of :text, :maximum => 400
 end
