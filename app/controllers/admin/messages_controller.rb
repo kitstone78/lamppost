562d1998d5d4ef272f1590e0b5059693c8c42996
Colin Stodd <cbstodd@gmail.com>
2014-08-07 13:37:35 -0400

    Created admin/messages views/controller

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,6 +1,42 @@
 class Admin::MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
 
+  def create
+    @message = message.new(params.require(:message).permit!)
+  if @message.save
+    redirect_to admin_message_path(@message), notice: "message #{@message.id} was created"
+  else
+    render 'new'
+    end
+  end
 
+  def show
+   @message = Message.find(params[:id])
+  end
 
+  def new
+    @message = Message.new
+  end 
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+  if @message.update(params.require(:message).permit!)
+    redirect_to admin_messages_path(@message), notice: "message #{@message.messagename} was updated successfully"
+  else
+    render 'edit'
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to admin_messages_path, notice: "message #{@message.id} was deleted}"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ resources :users
 
   namespace :admin do
     resources :users
+    resources :messages
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
