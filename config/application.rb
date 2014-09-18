e40f6352b55151c690ac9d70ca78fccb0f229c49
Sean Flaherty <sean.flaherty@gmail.com>
2014-09-18 15:56:35 -0400

    change to use for pry.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# Works with pry.  This might not be in the right place
+require 'pry-nav'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
